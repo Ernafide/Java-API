01de26e813c85b1e58aa775693bdd28b7f21bd10
Julia <Ernafide@gmail.com>
2023-03-10 18:56:05 +0300

    Api

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Post {
+    public org.example.FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
